4d52f047764c3d31e97194bf81f042bad5a98d87
Mayuresh Agashe <mayureshagashe2002@gmail.com>
2021-06-18 15:20:30 +0530

    create db visualization script

--- MySQL DB.py
+++ MySQL DB.py
@@ -1,4 +1,17 @@
+import mysql.connector
 
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='Mihirmayuresh123!',
+    database='scrapped_db'
+)
 
-# mycursor.execute('CREATE DATABASE scrapped_db')
+mycursor = db.cursor()
+
+Q2 = 'SELECT * FROM product'
+
+mycursor.execute(Q2)
+for x in mycursor:
+    print(x)
 
