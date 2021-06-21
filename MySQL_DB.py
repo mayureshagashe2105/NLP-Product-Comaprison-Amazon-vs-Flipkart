cbe372cb5f22c1abbf5b5f6c94e78d9523629e25
Mayuresh Agashe <mayureshagashe2002@gmail.com>
2021-06-21 09:04:21 +0530

    Extract similar objects and thier prices

--- MySQL_DB.py
+++ MySQL_DB.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import numpy as np
 
 db = mysql.connector.connect(
     host='localhost',
@@ -8,25 +9,46 @@ db = mysql.connector.connect(
 )
 
 mycursor = db.cursor(buffered=True)
+mycursor1 = db.cursor(buffered=True)
 
 
 def getamazon(shape):
-    Q2 = 'SELECT name FROM product WHERE isprime != "NA"'
-    mycursor.execute(Q2)
-    counter = 0
+    Q = 'SELECT name FROM product WHERE isprime != "NA"'
+    mycursor.execute(Q)
+    counter1 = 0
     for x in mycursor:
-        if counter == shape:
+        if shape == counter1:
             break
-        yield x[0]
-        counter += 1
+        else:
+            counter1 += 1
+            yield x[0]
 
 
 def getflipkart(shape):
-    Q2 = 'SELECT name FROM product WHERE isprime = "NA"'
-    mycursor.execute(Q2)
+    Q1 = 'SELECT name FROM product WHERE isprime = "NA"'
+    mycursor1.execute(Q1)
     counter = 0
-    for y in mycursor:
+    for y in mycursor1:
         if counter == shape:
             break
-        yield y[0]
-        counter += 1
+        else:
+            counter += 1
+            yield y[0]
+
+
+def retrivedata(ndarray_pairs):
+    Q = 'SELECT * FROM product WHERE isprime != "NA"'
+    mycursor.execute(Q)
+    counter0 = 0
+    Q1 = 'SELECT * FROM product WHERE isprime = "NA"'
+    _ = 0
+    for x in mycursor:
+        mycursor1.execute(Q1)
+        counter1 = 0
+        for y in mycursor1:
+            if [counter0, counter1] in ndarray_pairs.tolist():
+                _ += 1
+                print(_, '. ', x[1:3], '\n     ', y[1:3])
+                print('-------------------------------------------------')
+            counter1 += 1
+        counter0 += 1
